8fa873a2d33264980d23301d407349222f3b85e1
schwemro <rschwemmle@yahoo.de>
2017-07-07 18:10:35 +0100

    No major changes.

--- main.m
+++ main.m
@@ -10,13 +10,12 @@ clear classes;
 clc;
 
 %% generate artificial runoff time series
-
+tic
 approach=1; % 1 = 'AR' or 2 = 'ARMA'
 opt_val = zeros(10,1);
-set(0,'defaultFigureVisible','off')
-Qsyn = Qsynth(approach, '01-01-1995', '31-12-2015');
+Qsyn = Qsynth(approach, '01-01-2000', '31-12-2019');
 Qsyn.folderres('pre');
-Qsyn.importts('input/89006_95_02.csv', 'dd/MM/yyyy');
+Qsyn.importts('input/pfinz_00_06.csv', 'dd.MM.yyyy');
 Qsyn.perennial();
 Qsyn.tt_obs.Q = Qsyn.fillgaps(Qsyn.tt_obs.Q);
 Qsyn.tt_obs.Q_trans = Qsyn.logtransform(Qsyn.tt_obs.Q);
@@ -24,11 +23,14 @@ Qsyn.testseas();
 Qsyn.rmseas(Qsyn.Q_regime_daily, Qsyn.Q_std_daily);
 Qsyn.determineorder();
 Qsyn.selectmodel();
+Q_max = max(Qsyn.tt_obs.Q)*1.1;
+i=1;
 for i =1:10
+    prog = (i/10)*100;
+    disp([num2str(prog) '%'])
     Qsyn.folderres(num2str(i));
     Q_sim1 = Qsyn.generaterunoff(Qsyn.EstMdl,Qsyn.Q_regime_daily, Qsyn.Q_std_daily);
     Qsyn.tt_syn.Q_sim_re = Q_sim1;
-    Q_max = max(Qsyn.tt_obs.Q)*1.1;
     Q_cp = Qsyn.cutpeaksrgm(Q_max,Qsyn.EstMdl);
     Qsyn.tt_syn.Q_sim_re = Q_cp;
     Qsyn.optMAwMWS();
@@ -47,21 +49,23 @@ for i =1:10
     f = figure('Name','Q_obs vs Q_syn','NumberTitle','off','defaultFigureVisible','off');
     hold on
     plot(Qsyn.tt_obs.Date, Qsyn.tt_obs.Q, 'b');
-    plot(Qsyn.tt_syn.Date, Q_sim1, 'g--');
-    plot(Qsyn.tt_syn.Date, Q_cp, 'r--');
-    plot(Qsyn.tt_syn.Date, Q_ma, 'm');
+    plot(Qsyn.tt_syn.Date, Q_ma, 'r');
     hold off
-    h1 = [Qsyn.tt_syn.Date(1) Qsyn.tt_syn.Date(end)];
-    h2 = [Q_max Q_max];
-    line(h1,h2,'Color','black','LineStyle','-.')
+%     h1 = [Qsyn.tt_syn.Date(1) Qsyn.tt_syn.Date(end)];
+%     h2 = [Q_max Q_max];
+%     line(h1,h2,'Color','black','LineStyle','-.')
     grid;
     xlabel('Date');
     ylabel('Q [m^3/s]');
     xlim(datetime(2000,[1 12],[1 31]));
     xtickformat('dd-MMM-yyyy');
-    legend({'Q_{obs}','Q_{syn}','cutted peaks', 'MAwMWS'},'Box','off');
+    legend({'Q_{obs}','Q_{syn}'},'Box','off');
     saveas(f,[Qsyn.dir_results '/Q_obs_vs_Q_syn.fig']);
+    close all;
 end
 
 [val, idx] = min(opt_val);
-writetable(opt_val,'Delimiter','\t','opt_val.txt');
+disp(['Folder ' num2str(idx) ' shows the best results!'])
+dlmwrite('opt_val.txt',opt_val,'delimiter','\t','precision',3);
+t = round(toc/60,1);
+disp(['Total runtime: ' num2str(t) ' min'])
